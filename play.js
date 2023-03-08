e7c5005593ddc3e6fdbbb96335c0e52ecdea6436
wandakim <nwd1583@naver.com>
2023-03-09 02:31:26 +0900

    object

--- play.js
+++ play.js
@@ -1,14 +1,13 @@
-// Falshy 거짓인 값
-console.log(!!0);
-console.log(!!-0);
-console.log(!!'');
-console.log(!!null);
-console.log(!!undefined);
-console.log(!!NaN);
+const obj = {
+  name: '성빈',
+  age: 20,
+};
+// 코딩하는 시점에, 정적으로 접근이 확정됨
+obj.name;
+obj.age;
 
-// Truthy 참인 값
-console.log(!!1);
-console.log(!!-1);
-console.log(!!'text');
-console.log(!!{});
-console.log(!!Infinity);
+// 동적으로 속성에 접근하고 싶을때 대괄호 표기법 사용!
+function getValue(obj, key) {
+  return obj[key];
+}
+console.log(getValue(obj, 'name'));
