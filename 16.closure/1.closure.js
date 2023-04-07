53fccf055fd11291ce379ef721a64fcdd6447770
wandakim <nwd1583@naver.com>
2023-04-07 20:24:02 +0900

    closure

--- 16.closure/1.closure.js
+++ 16.closure/1.closure.js
@@ -1,8 +1,20 @@
+/* 
+함수와 함수 외부를 둘러싼 렉시컬 환경의 조합이다. 즉, 
+내부 함수에서 외부 함수의 스코프에 접근할 수 있는 것을 말한다. 
+블록 스코프와 비슷하지만 클로저는 함수에 대한 것을 말한다. 
+ */
+
 const text = 'hello';
 function func() {
-  console.log(text);
+  console.log(text); // 외부의 text에 접근 가능
 }
 func();
+
+/* 
+엄밀히 말하면 위와 같이 외부의 값에 접근할 수 있다고 해서 다 클로저라고 하지는 않고, 
+중첩된 함수에서 내부 함수가 외부 함수의 스코프에 있는 데이터에 접근할 수 있는 것을 말한다. 
+아래의 예를 살펴보자. 
+*/
 
 function outer() {
   const x = 0;
@@ -12,4 +24,4 @@ function outer() {
   return inner;
 }
 const func1 = outer();
-func1();
+func1(); // 클로저에 의해 함수가 리턴이 될 때 함수의 outer lexical 환경이 함께 리턴되므로 접근 가능하다.
--- 16.closure/2.why.js
+++ 16.closure/2.why.js
@@ -1,9 +1,15 @@
-// 내부 정보를 은닉하고, 공개 함수(public, 외부)를 통한 데이터 조작을 위해
-// 캡슐화와 정보은닉
-// 클래스 private 필드 또는 메소드를 사용하는 효과와 동일!
+/* 
+클로저의 활용예재를 살펴보자. 
+내부 정보를 은닉하고, 공개 함수(public, 외부)를 통한 데이터 조작을 위해 쓰인다. 
+즉, 캡슐화와 정보은닉을 위해 쓰인다. 
+클래스 private 필드 또는 메소드를 사용하는 효과와 동일하다. 
+이전 브라우저에서는 private과 class가 없었기 때문에 클로저의 특징을 활용해 정보를 은닉하고 
+공개 함수를 통해서만 데이터를 조작할 수 있게 하기 위해 쓰였다. 
+*/
 function makeCounter() {
-  let count = 0;
+  let count = 0; // count를 렉시컬 환경에 숨겨두고
   function increase() {
+    //외부에서는 increase라는 public 함수만을 사용해서 조작할 수 있게 했다.
     count++;
     console.log(count);
   }
@@ -13,6 +19,47 @@ const increase = makeCounter();
 increase();
 increase();
 increase();
+
+/* 
+클로저라는 특징을 활용해서 함수를 활용해 은닉하고자 하는 정보를 감추고 오직 퍼블릭 함수를 통해서만 
+내부 데이터를 조작할 수 있게 했다.  */
+{
+  // mdn에 있는 좋은 예제.
+  const counter = (function () {
+    let privateCounter = 0;
+    function changeBy(val) {
+      privateCounter += val;
+    }
+
+    // 외부에서는 이 세가지 함수만을 이용해서 내부 데이터를 조작할 수 있다.
+    return {
+      increment() {
+        changeBy(1);
+      },
+
+      decrement() {
+        changeBy(-1);
+      },
+
+      value() {
+        return privateCounter;
+      },
+    };
+  })();
+
+  console.log(counter.value()); // 0.
+
+  counter.increment();
+  counter.increment();
+  console.log(counter.value()); // 2.
+
+  counter.decrement();
+  console.log(counter.value()); // 1.
+}
+
+/* 
+하지만 더이상 이렇게 정보를 은닉할 필요는 없다. 이제 클래스를 사용하면 된다. 
+*/
 
 class Counter {
   #count = 0;
