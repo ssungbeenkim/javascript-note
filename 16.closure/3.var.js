2f010c31299029dc552443cec1dcb66d619f6e35
wandakim <nwd1583@naver.com>
2023-04-07 20:40:11 +0900

    var and scope

--- 16.closure/3.var.js
+++ 16.closure/3.var.js
@@ -1,6 +1,6 @@
 function loop() {
   const array = [];
-  // var과 let의 차이점!
+  // var i; => 이렇게 선언해 두는 것과 동일하게 작동한다.
   for (var i = 0; i < 5; i++) {
     array.push(function () {
       console.log(i);
@@ -10,4 +10,10 @@ function loop() {
 }
 
 const array = loop();
-array.forEach((func) => func());
+array.forEach((func) => func()); // 5 5 5 5 5
+
+/* 
+var로 선언된 변수는 함수 스코프만 된다. 안된다. 따라서 var로 선언된 변수는 
+loop() 스코프에서 1->5로 바뀌게 된다. 결국 array가 리턴 될 때 array 안의 요소로 들어있는 
+i를 출력하는 함수는 loop()함수 스코프와 함께 최종적으로 변경된 i의 정보를 출력하게 된다. 
+*/
