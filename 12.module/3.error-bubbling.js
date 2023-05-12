fb29dc76a41eb128e6a15cd23c3fb8f3d1096dfe
wandakim <nwd1583@naver.com>
2023-05-13 02:25:30 +0900

    Edit error-bubbling

--- 12.module/3.error-bubbling.js
+++ 12.module/3.error-bubbling.js
@@ -1,4 +1,6 @@
 // Bubbling up, Propagating 🧼
+
+// 에러는 최종적으로 호출한 곳까지 전달된다.
 function a() {
   throw new Error('error!');
 }
@@ -8,7 +10,7 @@ function b() {
     a();
   } catch (error) {
     console.log('생각해보니깐 이 에러는 내가 핸들링 할 수 없을 것 같군!');
-    throw error;
+    throw error; // 잡은 에러를 핸들린 하지 않고 에러를 다시 던진다.
   }
 }
 
